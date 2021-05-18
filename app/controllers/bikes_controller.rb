d1d039cb94560d6925f30a8714f9450e38735e69
sorayabenberghout <soraya.benberghout@gmail.com>
2021-05-18 17:56:34 +0200

    ajouter_index_bikes_controller_views

--- app/controllers/bikes_controller.rb
+++ app/controllers/bikes_controller.rb
@@ -1,2 +1,12 @@
 class BikesController < ApplicationController
+
+  def index
+    @bikes = Bike.all
+  end
+
+  private
+
+  def bike_params
+    params.require(:bike).permit(:description, :size, :color, :price)
+  end
 end
