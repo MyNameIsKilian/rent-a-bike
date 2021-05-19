bcc92d549e6ff11680391851508a697b359b501c
Kilian Alliot <kiliantravail@gmail.com>
2021-05-19 14:24:33 +0200

    map zoom edited

--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -4,7 +4,7 @@ import 'mapbox-gl/dist/mapbox-gl.css';
   const fitMapToMarkers = (map, markers) => {
   const bounds = new mapboxgl.LngLatBounds();
   markers.forEach(marker => bounds.extend([ marker.lng, marker.lat ]));
-  map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 });
+  map.fitBounds(bounds, { padding: 70, maxZoom: 14, duration: 0 });
 };
 
 const initMapbox = () => {
@@ -22,6 +22,7 @@ const initMapbox = () => {
         .setLngLat([ marker.lng, marker.lat ])
         .addTo(map);
     });
+    fitMapToMarkers(map, markers);
   }
 };
 
