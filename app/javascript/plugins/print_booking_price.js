7e0f386416ed88ad3a4e98cfb566a65a41c61c37
Valentina Banzet <valentina.banzet@gmail.com>
2021-05-20 14:39:50 +0200

    booking

---
+++ app/javascript/plugins/print_booking_price.js
@@ -0,0 +1,17 @@
+// const initBookingPrice = () => {
+//   //récupérer start date
+//   const start_date = document.querySelector('.booking_start_date')
+//   //récupérer end date
+//   const end_date = document.querySelector('.booking_end_date')
+//   console.log(start_date)
+//   console.log(end_date)
+//   //calculer la différence
+//   // const gap = (end_date - start_date)
+//   // //récupérer le price
+//   // const bike_price = document.getElementBy()
+//   // //on fait le price
+//   // const booking_price = (gap * bike_price)
+//   //on intègre le résultat en html
+// }
+
+// export { initBookingPrice };
